bb9c329cb6c9e68d7f449f56ee27ee3df13a38c1
Thirza Dado <70571921+tdado@users.noreply.github.com>
2022-09-18 16:25:31 +0200

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Break Out",
+        "excerpt":"Enter: start game, R: restart game, Escape: pause game.                                                    WebGL builds are not supported on mobile devices.                             ","categories": [],
+        "tags": [],
+        "url": "/misc/breakout/",
+        "teaser": null
+      },{
+        "title": "Procedural landscape generation",
+        "excerpt":"Procedurally-generated mountain-like terrain. It ends at infinity, so it never ends.   Click on any button to start playing.                                                    WebGL builds are not supported on mobile devices.                             ","categories": [],
+        "tags": [],
+        "url": "/misc/landscape/",
+        "teaser": null
+      },{
+        "title": "Neural decoding w/ synthesized reality",
+        "excerpt":"HYperrealistic reconstruction of PERceived faces from fMRI data 2022, 22 January Stimuli (top row) and their reconstructions from brain data (bottom row). Neural decoding seeks to find what information about a perceived external stimulus is present in the corresponding brain response. In particular, the original stimulus can be reconstructed based...","categories": [],
+        "tags": [],
+        "url": "/hyper/",
+        "teaser": "/assets/images/top-im.png"
+      },{
+        "title": "Brain2GAN-xl; Reconstructing perceived natural images with high variation from the primate brain via StyleGAN-xl",
+        "excerpt":"Abstract  TBA.   Full paper: TBA.    The 200 test set stimuli and their reconstructions from brain activity.  ","categories": [],
+        "tags": [],
+        "url": "/work/b2g-xl/",
+        "teaser": null
+      },{
+        "title": "Brain2GAN; Reconstructing perceived faces from the primate brain via StyleGAN",
+        "excerpt":"Abstract Neural coding characterizes the relationship between stimuli and their corresponding neural responses. The usage of synthesized yet photorealistic reality by generative adversarial networks (GANs) allows for superior control over these data: the underlying feature representations that account for the semantics in synthesized data are known a priori and their...","categories": [],
+        "tags": [],
+        "url": "/work/b2g/",
+        "teaser": null
+      },{
+        "title": "Hyperrealistic neural decoding for reconstructing faces from fMRI activations via the GAN latent space",
+        "excerpt":"Abstract Neural decoding can be conceptualized as the problem of mapping brain responses back to sensory stimuli via a feature space. We introduce (i) a novel experimental paradigm that uses well-controlled yet highly naturalistic stimuli with a priori known feature representations and (ii) an implementation thereof for HYPerrealistic reconstruction of...","categories": [],
+        "tags": [],
+        "url": "/work/hyper/",
+        "teaser": null
+      }]
