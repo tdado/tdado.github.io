8497092d0d9648cfa3922421d6ab3b692915ba4f
Thirza Dado <70571921+tdado@users.noreply.github.com>
2022-10-04 15:31:06 +0200

    Add files via upload

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,21 +1,27 @@
 var store = [{
         "title": "Break Out",
-        "excerpt":"Enter: start game, R: restart game, Escape: pause game.                                                    WebGL builds are not supported on mobile devices.                         Breakout         ","categories": [],
+        "excerpt":"Learn to implement your own game in Unity during the artificial intelligence master’s course Computer Graphics &amp; Computer Vision by Dr. Umut Güçlü at Radboud University. Lynn Le and myself are present as teaching assistants. Here, you can find my game: Enter: start game, R: restart game, Escape: pause game....","categories": [],
         "tags": [],
         "url": "/misc/breakout/",
         "teaser": null
       },{
         "title": "Procedural landscape generation",
-        "excerpt":"Procedurally-generated mountain-like terrain. It ends at infinity, so it never ends.   Click on any button to start playing.                                                    WebGL builds are not supported on mobile devices.                             ","categories": [],
+        "excerpt":"Learn to procedurally generate your own mountain-like terrain in Unity during the artificial intelligence master’s course Computer Graphics &amp; Computer Vision by Dr. Umut Güçlü at Radboud University. Lynn Le and myself are present as teaching assistants. Here, you can find my landscape: Click on any button in the window...","categories": [],
         "tags": [],
         "url": "/misc/landscape/",
         "teaser": null
       },{
         "title": "Neural decoding w/ synthesized reality",
-        "excerpt":"HYperrealistic reconstruction of PERceived faces from fMRI data 2022, 22 January Stimuli (top row) and their reconstructions from brain data (bottom row). Neural decoding seeks to find what information about a perceived external stimulus is present in the corresponding brain response. In particular, the original stimulus can be reconstructed based...","categories": [],
+        "excerpt":"2022, 22 January HYperrealistic reconstruction of PERceived faces from fMRI data Stimuli (top row) and their reconstructions from brain data (bottom row). Neural decoding seeks to find what information about a perceived external stimulus is present in the corresponding brain response. In particular, the original stimulus can be reconstructed based...","categories": [],
         "tags": [],
         "url": "/hyper/",
         "teaser": "/assets/images/top-im.png"
+      },{
+        "title": "Neural decoding of uncanniness",
+        "excerpt":"2022, 29 September An uncanny valley or -cliff? Written by Thirza Dado &amp; Umut Güçlü. The uncanny cliff hypothesizes that artificial figures remain either on the cliff (i.e., perceived as human-like) or they fall from the cliff (i.e., perceived as fake with disturbing feelings of eeriness and unease). Generative Adversarial Networks...","categories": [],
+        "tags": [],
+        "url": "/uncanny/",
+        "teaser": "/assets/images/synth2.png"
       },{
         "title": "Brain2GAN-xl; Reconstructing perceived natural images with high variation from the primate brain via StyleGAN-xl",
         "excerpt":"Abstract  TBA.   Full paper: TBA.    The 200 test set stimuli and their reconstructions from brain activity.  ","categories": [],
@@ -30,7 +36,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Hyperrealistic neural decoding for reconstructing faces from fMRI activations via the GAN latent space",
-        "excerpt":"Abstract Neural decoding can be conceptualized as the problem of mapping brain responses back to sensory stimuli via a feature space. We introduce (i) a novel experimental paradigm that uses well-controlled yet highly naturalistic stimuli with a priori known feature representations and (ii) an implementation thereof for HYPerrealistic reconstruction of...","categories": [],
+        "excerpt":"Thirza Dado, Yağmur Güçlütürk, Luca Ambrogioni, Gabriëlle Ras, Sander Bosch, Marcel van Gerven &amp; Umut Güçlü Abstract Neural decoding can be conceptualized as the problem of mapping brain responses back to sensory stimuli via a feature space. We introduce (i) a novel experimental paradigm that uses well-controlled yet highly naturalistic...","categories": [],
         "tags": [],
         "url": "/work/hyper/",
         "teaser": null
